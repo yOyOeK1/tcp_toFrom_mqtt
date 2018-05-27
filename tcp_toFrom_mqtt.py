4415a3840903892e017b607a4c74e144f0c16bef
yOyOeK1 <yoyoek@wp.pl>
2018-05-27 17:24:00 -0500

    damage control
    add little bit of damage control on connection lost

--- tcp_toFrom_mqtt.py
+++ tcp_toFrom_mqtt.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python2
 
 import paho.mqtt.client as paho
-import mosquitto
+#import mosquitto
 import time
 import thread
 import socket
-import os
+import os, signal
+import sys
 
 
 tcpIp = "127.0.0.1"
@@ -37,15 +38,12 @@ class interpreterObj:
 			self.printCorrectInfo(self.name)
 			self.doit(msg)
 
-	
-
 
 class interpreterMWV(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
 		self.send("sensors/AppWindAngle", v[1] ) 
 		self.send("sensors/AppWindSpeed", v[3] )
-
 class interpreterDPT(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
@@ -54,7 +52,6 @@ class interpreterDBT(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
 		self.send("sensors/Depth", v[1] ) 
-
 class interpreterRMC(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
@@ -63,17 +60,14 @@ class interpreterRMC(interpreterObj):
 		self.send("sensors/GPS/lon", v[5] )
 		self.send("sensors/GPS/sog", v[7] )
 		self.send("sensors/GPS/cog", v[8] )
-
 class interpreterHDM(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
 		self.send("sensors/flux", v[1] ) 
-
 class interpreterRSA(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
 		self.send("sensors/rudder", v[1] ) 
-
 class interpreterAPD(interpreterObj):
 	def doit(self,msg):
 		v = msg.split(",")
@@ -101,15 +95,10 @@ interpreters.append( interpreterAPD("APD") )
 
 
 
-mqtt = mosquitto.Mosquitto("id")
-mqtt.connect(mqttpIp, mqttpPort)
-def mqttSend( topic, msg ):
-	#cmd = "mosquitto_pub -h \"%s\" -p \"%s\" -t \"%s\" -m '%s'" % ( mqttpIp, mqttpPort, topic, msg)
-	#os.system(cmd)
-	mqtt.publish(topic, msg)
 
 
-def mqtt_on_message(client, userdata, message):
+
+def pmqtt_on_message(client, userdata, message):
 	toTcp = []
 
 	msg = str(message.payload.decode("utf-8"))
@@ -127,37 +116,49 @@ def mqtt_on_message(client, userdata, message):
 
 	cs.close()
 
+def mqtt_on_disconnect(client, userdata, message):
+	print "on disconnect !"
+	print "client[%s]"%client
+	print "userdata[%s]" %userdata
+	print "message[%s]" %message
+	os.kill(os.getpid(), signal.SIGTERM)
 
+def mqttSend( topic, msg ):
+	mqtt.publish(topic, msg)
 
-def mqttListin():
-	client = paho.Client("mqttToTcp")
-	client.on_message=mqtt_on_message
-	client.connect(mqttpIp)
-	printOut( "connected to mqtt" )
-	client.loop_start() #start loop to process received messages
-	client.subscribe("/des/mqttToTcp")#subscribe
-
-
-
+# tcp client 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((tcpIp, tcpPort))
-#client.send('$00UNO,?'+"\n\r")
 
-thread.start_new_thread( mqttListin, () )
+# mqtt client 
+mqtt = paho.Client("mqttPusherToMqtt")
+mqtt.connect(mqttpIp)
+mqtt.on_message=pmqtt_on_message
+mqtt.on_disconnect=mqtt_on_disconnect
+printOut( "connected to mqtt" )
+mqtt.loop_start()
+mqtt.subscribe("/des/mqttToTcp")
 
+erro = 10
 
 while True:
+	time.sleep(0.01)
 	response = client.recv(4096)
 	for l in ("%s"%response).splitlines():
-		try:
-			for p in interpreters:
-				p.parse(l)
-			mqttSend("/des/raw",l)
-			#thread.start_new_thread( mqttSend, ("/des/raw", l ) )
-		except:
-   			printOut( "problem with starting thread" )
+		for p in interpreters:
+			p.parse(l)
+			mqttSend("/des/raw",l)			
 		
-		#print "line:["+l+"] len:["+("%s"%len(l))+"]"
 	
+	if len(response) == 0:
+		erro-= 1
+		print "response == 0 !"
+		time.sleep(.5)
+	else:
+		erro = 10
+				
+	if erro < 0:
+		print "connection lost to tcp server - maby :P"
+		os.kill(os.getpid(), signal.SIGTERM)
 
 print "main DONE"
